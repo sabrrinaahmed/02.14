84d7286f27fde874205d7af293fc393d1aaba18a
sabrrinaahmed <sabrrinaahmed@tamu.edu>
2022-01-31 10:58:41 -0600

    start

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string dummy = "";
+    int code = 0;
+
+    cout << "(sabs) Hi Shawty, I love you. Enter your code number: ";
+    cin >> code;
+    getline(cin, dummy);
+
+    cout << "(computer) Your code number is: " << code << endl;
+}
